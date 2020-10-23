4b7792a7018139e9c7281d2f77798fa9674a5014
abdessamad <abdessamadhamzaoui1@gmail.com>
2020-10-24 00:19:09 +0100

    new passport model

--- Index.js
+++ Index.js
@@ -1,42 +1,95 @@
 //const server1 = require("./Server");
-const app = require('express')();
-require('dotenv').config();
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise
-mongoose.connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => {
-    console.log('MongoDB Connected…')
-})
-.catch(err => console.log(err));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.listen(3333, () => {
-console.log(`Server running at here 3333`);
-});
-const { auth } = require('./middleware/auth')
-const { RegisterUser, LoginUser, LogoutUser,getUserDetails } = require('./controller/authcontroller');
-app.post('/api/users/register',RegisterUser);
-app.post('/api/users/login',LoginUser);
-app.get('/api/users/auth',auth,getUserDetails);
-app.get('/api/users/logout', auth, LogoutUser);
+var express = require("express"),
+    mongoose = require("mongoose"),
+    bodyParser = require("body-parser"),
+    User = require("./models/user"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose");
+
+
+
+
+    mongoose.connect("mongodb+srv://admin:123@cluster0.hloif.mongodb.net/TP1?retryWrites=true&w=majority", { useNewUrlParser: true });    
+    var app = express();
+    app.set("view engine","ejs");
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(require("express-session")({
+        secret:"Miss white is my cat",
+        resave: false,
+        saveUninitialized: false
+    }));
+    
+    app.use(passport.initialize());
+    app.use(passport.session());
+    
+    passport.use(new LocalStrategy(User.authenticate()));
+    passport.serializeUser(User.serializeUser());
+    passport.deserializeUser(User.deserializeUser());
+    
+    
+    
+    
+    app.get("/",function(req, res){
+        res.render("home"); 
+    });
+    
+    app.get("/secret", isLoggedIn, function(req, res){
+        res.render("secret");
+    });
+    
+    app.get("/register", function(req, res){
+        res.render("register");
+    });
+    
+    // handeling user sign up
+    app.post("/register", function(req, res){
+        console.log(req.body.username);
+        console.log(req.body.password);
+        User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+            if(err){
+                console.log(err);
+                return res.render("register");
+            }
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secret");
+            });
+        });
+    });
+    
+    // Login Form
+    app.get("/login", function(req, res){
+        res.render("login");
+    });
+    
+    // Login Logic
+    // middleware
+    app.post("/login", passport.authenticate("local",{
+        successRedirect: "/secret",
+        failureRedirect: "/login"
+    }), function(req, res){
+        console.log(req.body.username);
+    });
+    
+    // Logout
+    app.get("/logout", function(req, res){
+        req.logout();
+        res.redirect("/");
+    });
+    
+    // check isLoggedIn
+    function isLoggedIn(req, res, next){
+        if(req.isAuthenticated()){
+            return next();
+        }
+        res.redirect("/login");
+    }
+    
+    
+
 
 var requestHandlers = require("./RequestHandlers");
 
-
-
-
-app.get('/', (req, res) => {
-    requestHandlers.start(res)
-});
-app.get('/start', (req, res) => {
-    requestHandlers.start(res)
-});
 app.get('/upload', (req, res) => {
     requestHandlers.upload(res)
 });
@@ -52,7 +105,9 @@ app.get('/logout', (req, res) => {
 });
 
 
-
+app.listen(3333, function(){
+    console.log('started app')
+});
 //server1.start(router.route, handle);
 /*app.get("/start" ,requestHandlers.start(res));
 app.get("/upload",requestHandlers.upload(res));
