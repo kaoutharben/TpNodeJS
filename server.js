b18e8d0bccb805e100fcce99d7d8032f7b6f1fbf
Ascour <65546407+Ascour@users.noreply.github.com>
2020-10-25 22:04:09 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const app = require('./app');
+
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000);
+
+
+
+// nodemon sert à faire en sorte que le serveur exécute les modifications en temps réel
+
+
+
+
