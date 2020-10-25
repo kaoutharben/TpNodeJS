19c92576620a9ad984765b3ce52431f03b372d46
Ascour <65546407+Ascour@users.noreply.github.com>
2020-10-25 21:52:24 +0100

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -40,3 +40,16 @@ exports.signup = (req, res, next) => {
       })
       .catch(error => res.status(500).json({ error }));
   };
+
+
+exports.logout = (req, res, next) => {
+    try {
+      req.user.tokens = req.user.tokens.filter((token) =>{
+       return token.token !== req.token 
+      })
+      await req.user.save()
+      res.send()
+  } catch (error) {
+      res.status(500).send()
+  }
+  };
