1859c8c5ba20dddd623097af3bdeb376c39e2f14
Ascour <65546407+Ascour@users.noreply.github.com>
2020-10-25 21:56:46 +0100

    Update stuff.js

--- routes/stuff.js
+++ routes/stuff.js
@@ -11,7 +11,6 @@ const stuffCtrl = require('../controllers/stuff');
 router.get('/', auth, stuffCtrl.getAllStuff);
 router.post('/', auth, multer, stuffCtrl.createThing);// l'ordre est important il faut mettre auth avant tout pour que l'authentification se fasse avant toute chose
 router.get('/:id', auth, stuffCtrl.getOneThing); // /:id veut dire que l'id est dynamique
-router.put('/:id', auth, multer, stuffCtrl.modifyThing);
 router.delete('/:id', auth, multer, stuffCtrl.deleteThing);
 
 module.exports = router;
@@ -19,37 +18,3 @@ module.exports = router;
 
 
 
-/*
-
-
-
-// sert à envoyer des infos à la bd
-router.post('/', );
-
-
-
-// pour modifier une objet 
-router.put('/:id', (req, res, next) => {
-    
-  });
-
-  
-// supprimer un objet 
-router.delete('/:id', (req, res, next) => {
-    
-  });  
-  
-  
-  
-// pour trouver un objet avec son id
-router.get('/:id', (req, res, next) => { // :id montre que le id est dynamique
-  });
-
-
-// sert à donner une réponse si on ne l'utilise pas on aura une erreur c'est une middleware
-// montre les objets de la db
-router.get('/', (req, res, next) => { // /api/stuff indique la route à suivre 
-    
-  });
-
-*/
