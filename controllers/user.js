005030877f10e531e60786d6858fef0f09a62ee6
Ascour <65546407+Ascour@users.noreply.github.com>
2020-10-25 21:54:46 +0100

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-
+const bcrypt = require('bcrypt');
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
